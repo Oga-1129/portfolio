bde03647bd22491cbd6ed350d3d39559c15aa0cb
eleline <ggggepicstar@gmail.com>
2019-06-06 14:45:00 +0900

    Contactモーダルにメールフォームを追加

---
+++ eleline/send.php
@@ -0,0 +1,24 @@
+<?php
+
+$title = $_POST['title'];
+$mail = $_POST['mail'];
+$contents = $_POST['contents'];
+
+$contents = "ポートフォリオサイトからのコンタクト\n\n- 送り主メールアドレス -\n" . $mail . "\n\n- 本文 -\n" . $contents; 
+
+$to = "ggggepicstar@gmail.com";
+
+header('Content-Type: application/json; charset=utf-8');
+
+// 日本語設定
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+
+// メール送信
+if(mb_send_mail($to, $title, $contents)){
+  echo json_encode("true");
+} else {
+  echo json_encode("false");
+};
+
+exit;
